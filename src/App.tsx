3814f8d23764d464f2d8fecf536aa93b609e50b0
Jan Rybka <jan.rybka@britishcouncil.org>
2019-02-28 17:04:35 +0100

    Added notification functionality.

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,27 @@ import './App.css';
 import logo from './logo.svg';
 import AppTester from './components/AppTester';
 
-class App extends React.Component {
+class App extends React.Component {  
+  componentDidMount() {
+    this.notificationSetup();
+  }
+
+  notificationSetup() {
+    // Let's check if the browser supports notifications
+    if (!("Notification" in window)) {
+        console.log("This browser does not support system notifications");
+    }
+    // Otherwise, we need to ask the user for permission
+    else if (Notification.permission !== "denied") {
+      Notification.requestPermission(function(permission: string) {
+        // If the user accepts, let's create a notification
+        if (permission === "granted") {
+          new Notification(`Health Check Monitor ready to go.`);
+        }
+      });
+    }
+  }
+
   public render() {
     return (
       <div className="App">
@@ -12,18 +32,19 @@ class App extends React.Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Health Check Monitor</h1>
         </header>
-        <AppTester name="develop CI" url="nppapi-develop-ci.britishcouncil.org/api/healthcheck?token=7b5f1937-f875-4619-b5e5-1686bfd9f58f" />
-        <AppTester name="develop QA" url="nppapi-develop-qa.britishcouncil.org/api/healthcheck?token=7b5f1937-f875-4619-b5e5-1686bfd9f58f" />
-        <AppTester name="release CI" url="nppapi-release-ci.britishcouncil.org/api/healthcheck?token=7b5f1937-f875-4619-b5e5-1686bfd9f58f" />
-        <AppTester name="release QA" url="nppapi-release-qa.britishcouncil.org/api/healthcheck?token=7b5f1937-f875-4619-b5e5-1686bfd9f58f" />
-        <AppTester name="demo CI" url="nppapi-demo-ci.britishcouncil.org/api/healthcheck?token=7b5f1937-f875-4619-b5e5-1686bfd9f58f" />
-        <AppTester name="demo QA" url="nppapi-demo-qa.britishcouncil.org/api/healthcheck?token=7b5f1937-f875-4619-b5e5-1686bfd9f58f" />
+        <AppTester name="develop CI" url="nppapi-develop-ci.dev.britishcouncil.org/api/healthcheck?token=7b5f1937-f875-4619-b5e5-1686bfd9f58f" />
+        <AppTester name="develop QA" url="nppapi-develop-qa.dev.britishcouncil.org/api/healthcheck?token=7b5f1937-f875-4619-b5e5-1686bfd9f58f" />
+        <AppTester name="develop Demo" url="nppapi-develop-demo.dev.britishcouncil.org/api/healthcheck?token=7b5f1937-f875-4619-b5e5-1686bfd9f58f" />
         <AppTester name="UAT" url="nppapi-uat.britishcouncil.org/api/healthcheck?token=7b5f1937-f875-4619-b5e5-1686bfd9f58f" />        
         <AppTester name="Staging" url="nppapi-staging.britishcouncil.org/api/healthcheck?token=7b5f1937-f875-4619-b5e5-1686bfd9f58f" />
       </div>
     );
 
     //<AppTester name="Live" url="nppapi.britishcouncil.org/api/healthcheck?token=7b5f1937-f875-4619-b5e5-1686bfd9f58f" />
+    //<AppTester name="release CI" url="nppapi-release-ci.dev.britishcouncil.org/api/healthcheck?token=7b5f1937-f875-4619-b5e5-1686bfd9f58f" />
+    //<AppTester name="release QA" url="nppapi-release-qa.dev.britishcouncil.org/api/healthcheck?token=7b5f1937-f875-4619-b5e5-1686bfd9f58f" />
+    //<AppTester name="release Demo" url="nppapi-release-demo.dev.britishcouncil.org/api/healthcheck?token=7b5f1937-f875-4619-b5e5-1686bfd9f58f" />
+        
   }
 }
 
--- src/components/AppTester.tsx
+++ src/components/AppTester.tsx
@@ -1,127 +1,187 @@
-import * as React from 'react';
-import axios from 'axios';
-import TestResult from './TestResult';
+import * as React from "react";
+import axios from "axios";
+import TestResult from "./TestResult";
+declare var Notification: any;
 
 export interface AppTesterProps {
-    name: string;
-    url: string;
+  name: string;
+  url: string;
 }
 
 interface HcTestResult {
-    testDate: string;
-    allChecksPassed: boolean;
-    namesOfFailedTests: string[];
-    rawResult: string;
+  testDate: string;
+  allChecksPassed: boolean;
+  namesOfFailedTests: string[];
+  rawResult: string;
 }
 
 interface AppTesterState {
-    lastTests: HcTestResult[];
-    intervalId?: any;
-    callIsRunning: boolean;
-    historyShown: boolean;
+  lastTests: HcTestResult[];
+  intervalId?: any;
+  callIsRunning: boolean;
+  historyShown: boolean;
 }
 
-export default class AppTester extends React.Component<AppTesterProps, AppTesterState> {
-    constructor(props: AppTesterProps) {
-        super(props);
-        this.state = { lastTests: [], callIsRunning: false, historyShown: false };
+export default class AppTester extends React.Component<
+  AppTesterProps,
+  AppTesterState
+> {
+  constructor(props: AppTesterProps) {
+    super(props);
+    this.state = { lastTests: [], callIsRunning: false, historyShown: false };
+  }
+
+  componentDidMount() {
+    this.makeHealthCheck();
+    var intervalId = setInterval(this.makeHealthCheck.bind(this), 30000);
+    // store intervalId in the state so it can be accessed later:
+    this.setState({ intervalId: intervalId });
+  }
+
+  componentWillUnmount() {
+    if (this.state.intervalId != null) {
+      clearInterval(this.state.intervalId);
+    }
+  }
+
+  render() {
+    const { name, url } = this.props;
+    let latestTest = null;
+    let pendingRequest = null;
+    if (this.state.callIsRunning) {
+      pendingRequest = <img src="img/loader.gif" width="32" height="32" />;
     }
 
-    componentDidMount() {
-        this.makeHealthCheck();
-        var intervalId = setInterval(this.makeHealthCheck.bind(this), 30000);
-        // store intervalId in the state so it can be accessed later:
-        this.setState({ intervalId: intervalId });
+    let tests = this.state.lastTests;
+    if (this.state.lastTests.length > 0) {
+      let lt = tests[0];
+      latestTest = (
+        <>
+          <TestResult
+            allChecksPassed={lt.allChecksPassed}
+            testDate={lt.testDate}
+            namesOfFailedTests={lt.namesOfFailedTests}
+            rawResult={lt.rawResult}
+          />
+        </>
+      );
     }
 
-    componentWillUnmount() {
-        if (this.state.intervalId != null) {
-            clearInterval(this.state.intervalId);
-        }
+    let history = null;
+    if (this.state.historyShown) {
+      const nodes = tests.map(test => (
+        <TestResult
+          allChecksPassed={test.allChecksPassed}
+          testDate={test.testDate}
+          namesOfFailedTests={test.namesOfFailedTests}
+          rawResult={test.rawResult}
+        />
+      ));
+      history = <div className="history">{nodes}</div>;
     }
 
-    render() {
-        const { name, url } = this.props;
-        let latestTest = null;
-        let pendingRequest = null;
-        if (this.state.callIsRunning) {
-            pendingRequest = <img src="img/loader.gif" width="32" height="32" />
-        }
+    return (
+      <div className="app-tester">
+        {pendingRequest}
+        <a href={"http://" + url} target="_blank">
+          {name}
+        </a>
+        {latestTest}
+        <button onClick={this.toogleHistory}>
+          {this.state.historyShown ? "Hide history" : "Show history"}
+        </button>
+        {history}
+      </div>
+    );
+  }
 
-        let tests = this.state.lastTests;
-        if (this.state.lastTests.length > 0) {
-            let lt = tests[0];
-            latestTest = <>
-                <TestResult allChecksPassed={lt.allChecksPassed} testDate={lt.testDate} namesOfFailedTests={lt.namesOfFailedTests} rawResult={lt.rawResult} />
-                
-            </>
-        }
+  toogleHistory = (): void => {
+    this.setState({
+      ...this.state,
+      historyShown: !this.state.historyShown
+    });
+  };
 
-        let history = null;
-        if (this.state.historyShown) {
-            const nodes = tests.map(test =>
-                <TestResult allChecksPassed={test.allChecksPassed} testDate={test.testDate} namesOfFailedTests={test.namesOfFailedTests} rawResult={test.rawResult} />
-            )
-            history = <div className='history'>{nodes}</div>;
-        }
+  notifyMe() {
+    // Let's check if the browser supports notifications
+    if (!("Notification" in window)) {
+      console.log("This browser does not support system notifications");
+    }
+    // Let's check whether notification permissions have already been granted
+    else if (Notification.permission === "granted") {
+      // If it's okay let's create a notification
+      new Notification(`${this.props.name} environment doesn't work`);
+    }
+  }
 
-        return (
-            <div className='app-tester'>
-                {pendingRequest}
-                <a href={"http://" + url} target="_blank">{name}</a>
-                {latestTest} 
-                <button onClick={this.toogleHistory}>{this.state.historyShown ? "Hide history" : "Show history"}</button>
-                {history}
-            </div>
-        );
+  makeHealthCheck(): void {
+    const url = this.props.url;
+
+    if (this.state.callIsRunning) {
+      return;
     }
 
-    toogleHistory = (): void => {
+    this.setState({
+      ...this.state,
+      callIsRunning: true
+    });
+
+    axios
+      .get("http://localhost:8080/" + url)
+      .then(
+        function(response) {
+          let result: HcTestResult;
+          //data validation
+          if (!response.data || !response.data.checksDetails) {
+            result = {
+              testDate: new Date().toISOString(),
+              allChecksPassed: false,
+              rawResult: JSON.stringify(response.data),
+              namesOfFailedTests: ["all"]
+            };
+            return result;
+          }
+
+          result = {
+            testDate: new Date().toISOString(),
+            allChecksPassed: response.data.allChecksPassed,
+            rawResult: JSON.stringify(response.data),
+            namesOfFailedTests: response.data.checksDetails
+              .filter((test: any) => test.testPassed == false)
+              .map((test: any) => test.name)
+          };
+          return result;
+        },
+        reason => {
+          let result: HcTestResult = {
+            testDate: new Date().toISOString(),
+            allChecksPassed: false,
+            rawResult: JSON.stringify(reason),
+            namesOfFailedTests: ["all"]
+          };
+          return result;
+        }
+      )
+      .then(data => {
+        let prevTests = this.state.lastTests;
+        if (prevTests.length > 50) prevTests.pop();
+        let tests = [data, ...prevTests];
+
+        if (
+          data != undefined &&
+          !data.allChecksPassed &&
+          prevTests.length > 0 &&
+          prevTests[0].allChecksPassed
+        ) {
+          // notification
+          this.notifyMe();
+        }
+
         this.setState({
-            ...this.state,
-            historyShown: !this.state.historyShown
+          ...this.state,
+          lastTests: tests,
+          callIsRunning: false
         });
-    }
-
-    makeHealthCheck(): void {
-        const url = this.props.url;
-        if (this.state.callIsRunning) {
-            return;
-        }
-        this.setState({
-            ...this.state,
-            callIsRunning: true
-        });
-        axios.get('http://localhost:8080/' + url)
-            .then(function (response) {
-                let result: HcTestResult = {
-                    testDate: new Date().toISOString(),
-                    allChecksPassed: response.data.allChecksPassed,
-                    rawResult: JSON.stringify(response.data),
-                    namesOfFailedTests: response.data.checksDetails.filter((test: any) => test.testPassed == false).map((test: any) => test.name),
-                };
-                return result;
-            },
-                reason => {
-                    let result: HcTestResult = {
-                        testDate: new Date().toISOString(),
-                        allChecksPassed: false,
-                        rawResult: JSON.stringify(reason),
-                        namesOfFailedTests: ["all"],
-                    };
-                    return result;
-                })
-            .then(data => {
-                let prevTests = this.state.lastTests;
-                if (prevTests.length > 50)
-                    prevTests.pop();
-                let tests = [data, ...prevTests];
-
-                this.setState({
-                    ... this.state,
-                    lastTests: tests,
-                    callIsRunning: false,
-                });
-            });
-    }
+      });
+  }
 }
